df5f268845daf750273556db7880644dba503297
mkaramihaylova <78750045+mkaramihaylova@users.noreply.github.com>
2022-03-23 10:57:35 -0400

    added more tests

--- bul_g2p_test.py
+++ bul_g2p_test.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-"""Unit tests for Bulgarian G2P."""
+"""Unit tests for Bulgarian g2p."""
 
 import unittest
 
@@ -9,25 +9,21 @@ import bul_g2p
 
 class G2PTest(unittest.TestCase):
     def rewrites(self, istring: str, expected_ostring: str) -> None:
- 
+
         ostring = bul_g2p.g2p(istring)
         self.assertEqual(ostring, expected_ostring)
-
 
     def test_abdication(self):
         self.rewrites("абдикацията", "abdikatsijatə")
 
-    def test_senior(self):
-        self.rewrites("абитуриентският", "abiturienskijat")
-
     def test_authority(self):
-        self.rewrites("авторитетният", "aftoritetnijat")
+        self.rewrites("авторитетният", "aftoritɛtnijat")
 
     def test_bombard(self):
         self.rewrites("бомбардиралото", "bombardiraɫoto")
 
     def test_dinner(self):
-        self.rewrites("вечеряхте", "vetʃerjaxte")
+        self.rewrites("вечеряхте", "vɛtʃɛrjaxtɛ")
 
     def test_homem(self):
         self.rewrites("въвежданията", "vəvɛʒdanijatə")
@@ -36,39 +32,47 @@ class G2PTest(unittest.TestCase):
         self.rewrites("господарствай", "ɡospodarstvaj")
 
     def test_border(self):
-        self.rewrites("граничещото", "ɡranitʃeʃtoto")
+        self.rewrites("граничещото", "ɡranitʃɛʃtoto")
 
     def test_chew(self):
-        self.rewrites("дъвчете", "dəftʃete")
+        self.rewrites("дъвчете", "dəftʃɛtɛ")
 
     def test_extract(self):
-        self.rewrites("извличайте", "izvlitʃajte")
+        self.rewrites("извличайте", "izvlitʃajtɛ")
 
     def test_crowning(self):
         self.rewrites("коронясващото", "koronjasvaʃtoto")
 
     def test_callers(self):
-        self.rewrites("наричащите", "naritʃaʃtite")
+        self.rewrites("наричащите", "naritʃaʃtitɛ")
 
     def test_resulted(self):
-        self.rewrites("произтечеха", "proistɛtʃexə")
+        self.rewrites("произтечеха", "proistɛtʃɛxə")
 
     def test_cry(self):
         self.rewrites("разплачат", "raspɫatʃat")
 
-    def test_slept(self):
-        self.rewrites("спяхме", "spjaxme")
+    def test_lied(self):
+        self.rewrites("лъженията", "ɫəʒɛnijatə")
 
-    def test_go(self):
-        self.rewrites("тръгваш", "trəgvaʃ")
+    def test_produce(self):
+        self.rewrites("възпроизвеждащото", "vəsproizvɛʒdaʃtoto")
 
-    def test_bite(self):
-        self.rewrites("хапех", "xapex")
+    def test_contribute(self):
+        self.rewrites("допринасящата", "doprinasjaʃtatə")
 
-    def test_hear(self):
-        self.rewrites("чу", "tʃu")
+    def test_sweep(self):
+        self.rewrites("замитал", "zamitaɫ")
+
+    def test_sent(self):
+        self.rewrites("пращалата", "praʃtaɫatə")
+
+    def test_divine(self):
+        self.rewrites("предсказвалият", "prɛtskazvalijat")
+
+    def test_spread(self):
+        self.rewrites("разпростирахте", "rasprostiraxtɛ")
 
 
-        
 if __name__ == "__main__":
     unittest.main()
